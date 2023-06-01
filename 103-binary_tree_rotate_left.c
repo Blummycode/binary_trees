668dcb103670d2090aae7c2c674d875ce49e1a0f
Blummycode <simonresearcher@gmail.com>
2023-06-01 03:17:13 -0700

    rotate left fix

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -5,30 +5,33 @@
  *
  * @tree: A pointer to the root node of the tree to rotate.
  *
- * Return: Pointer to the new root node of the tree once rotated
- *         NULL upon failure
+ * Return: A pointer to the new root node of the tree once rotated.
+ * 	   NULL upon failure
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	binary_tree_t *tmp = NULL, *parent;
 
-	if (!tree || !tree->right)
+	binary_tree_t *pivot, *tmp;
+
+	if (tree == NULL || tree->right == NULL)
 		return (NULL);
-	tmp = tree;
-	parent = tree->parent;
-	tree = tree->right;
-	tree->parent = NULL;
-	if (tree->left)
+
+	pivot = tree->right;
+	tmp = pivot->left;
+	pivot->left = tree;
+	tree->right = tmp;
+	if (tmp != NULL)
+		tmp->parent = tree;
+	tmp = tree->parent;
+	tree->parent = pivot;
+	pivot->parent = tmp;
+	if (tmp != NULL)
 	{
-		tmp->right = tree->left;
-		tree->left->parent = tmp;
+		if (tmp->left == tree)
+			tmp->left = pivot;
+		else
+			tmp->right = pivot;
 	}
-	else
-		tmp->right = NULL;
-	tmp->parent = tree;
-	tree->left = tmp;
-	if (parent)
-		parent->right = tree;
-	tree->parent = parent;
-	return (tree);
+
+	return (pivot);
 }
